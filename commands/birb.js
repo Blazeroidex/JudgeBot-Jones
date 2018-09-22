17595504e310a4077079ee5242aa34928073a83d
Glitch (judgebot-jones) <none>
2018-09-22 23:57:29 +0000

    🚓🌘 Checkpoint ./commands/birb.js:1546624/88

--- commands/birb.js
+++ commands/birb.js
@@ -1,11 +1,13 @@
 const snek = module.require("snekfetch");
-const api = "http://shibe.online/api/shibes";
+const api = "https://random.birb.pw/tweer/random";
 
 module.exports.run = async (bot, message, args) => {
   let msg = await message.channel.send("Generating...");
   
   let file = (await snek.get(api)).body.file;
   if(!file) return message.channel.send("Sorry, couldn't find one this time. Try again!");
+  
+  console.log(file)
 }
 
 module.exports.help = {
