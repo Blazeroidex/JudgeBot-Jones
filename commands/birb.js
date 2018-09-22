e0fcc3e0560f4a6822fdefb3f665039b19e621b4
Glitch (judgebot-jones) <none>
2018-09-22 23:47:00 +0000

    🏊🏼😳 Checkpoint ./commands/birb.js:1546624/690

--- commands/birb.js
+++ commands/birb.js
@@ -1,1 +1,15 @@
-const snek = module.require("")
+const snek = module.require("snekfetch");
+const api = "http://shibe.online/api/shibes";
+
+module.exports.run = async (bot, message, args) => {
+  let msg = await message.channel.send("Generating...");
+  
+  let file = (await snek.get(api)).body.file;
+  if(!file) return message.channel.send("Sorry, couldn't find one this time. Try again!");
+}
+
+module.exports.help = {
+  name: "birb",
+  description: "Sends a random picture of a birb",
+  usage: "!birb"
+}
