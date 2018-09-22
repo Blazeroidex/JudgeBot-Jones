061c97e9fe671a8da14812ac86bc50665c2d2b9b
Glitch (judgebot-jones) <none>
2018-09-22 23:25:47 +0000

    🌾💃 Checkpoint ./commands/birb.js:1546624/107 ./commands/shibe.js:1546624/1053

--- commands/birb.js
+++ commands/birb.js
@@ -5,14 +5,14 @@ const config = require("/app/config.json");
 module.exports.run = async (bot,message,args) => {
   
   let {body} = await superagent
-  .get(`https://shibe.online/api/birds`);
+  .get(`http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=false`);
   
-  let embed = new Discord.RichEmbed()
+  let embed = new Discord.MessageEmbed()
   .setColor("ff9900")
   .setTitle("Birb")
   .setImage(body.url);
   
-  message.channel.send({embed});
+  message.channel.send(embed);
     
 }
 
